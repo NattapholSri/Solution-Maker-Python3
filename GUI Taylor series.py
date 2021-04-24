9ac2cfab1bf40e1bd6ee1bc5eb75262fce1f6f94
NattapholSri <52815372+NattapholSri@users.noreply.github.com>
2021-04-24 15:28:49 +0700

    Usable GUI
    UI become fully function

--- GUI Taylor series.py
+++ GUI Taylor series.py
@@ -1,6 +1,53 @@
 from tkinter import *
 from tkinter import scrolledtext
-from Taylor_series import Taylor_series_with_subs
+import sympy as sym
+
+# custom function for GUI
+def Taylor_series_with_subs(func,n_max,n=0,view_lm=-1,subs_v=None):
+    # translate string expression to function
+    func = sym.sympify(func)
+
+    # check limit view
+    if view_lm == -1:
+        view_lm = n_max+1
+
+    # recursive from first to the end of 'n'
+    x = sym.symbols("x")
+    if n >= n_max:
+        diff_now = sym.diff(func)
+        sol = diff_now.subs(x,0)
+        final_level = sol * ((x**n) / sym.factorial(n))
+        if n <= view_lm:
+            out.insert(END,'diff f' + str(n) + ": " + str(diff_now))
+            out.insert(END," -> f(n)(0) * x**n / n! value in this level: " + str(final_level) + " \n")
+            out.insert(END,"\n")
+        return final_level
+    elif n == 0:
+        sol = func.subs(x,0)
+        this_level = sol * ((x**n) / sym.factorial(n))
+        n = n+1
+        if n <= view_lm:
+            out.insert(END,"no diff " + str(func))
+            out.insert(END," -> f(n)(0) * x**n / n! value in this level: " + str(this_level) + " \n")
+
+        if subs_v == None or subs_v == "":
+            func = this_level + Taylor_series_with_subs(func, n_max, n, view_lm)
+            out.insert(END,"\nFull Form: " + str(func))
+            return func
+        else:
+            func = this_level + Taylor_series_with_subs(func,n_max,n,view_lm)
+            out.insert(END,"\nFull Form: " + str(func))
+            out.insert(END,"\nsubstituted value = " + str(func.subs(x,subs_v)))
+            return func
+    else:
+        diff_now = sym.diff(func)
+        sol = diff_now.subs(x,0)
+        n = n+1
+        this_level = sol * ((x**n) / sym.factorial(n))
+        if n <= view_lm:
+            out.insert(END,'diff f' + str(n) + ":" + str(diff_now))
+            out.insert(END," -> f(n)(0) * x**n / n! value in this level: " + str(this_level) + " \n")
+        return this_level + Taylor_series_with_subs(diff_now,n_max,n,view_lm)
 
 # Preload function
 def limit_view_set():
@@ -12,15 +59,17 @@ def limit_view_set():
         return int(lm_view.get())
 
 def run_taylor():
-    tx_result = Taylor_series_with_subs(expr_str.get(),int(n_str.get()),0,limit_view_set(),x_str.get())
-    out.delete('1.0',END)
-    out.insert(END,str(tx_result ))
+    out.delete('1.0', END)
+    out.insert(END, "show Solution:\n")
+    Taylor_series_with_subs(expr_str.get(),int(n_str.get()),0,limit_view_set(),x_str.get())
+
+    out.insert(END,"\n\nDone!")
 
 # GUI General config
 
 window = Tk()
 window.title("Taylor's Series (Solution mk Programs)")
-window.geometry('640x480')
+window.geometry('800x600')
 
 # expression input zone
 
@@ -56,7 +105,7 @@ exe_btn.grid(column=2, row=6)
 
 
 # output zone
-out = scrolledtext.ScrolledText(window, height = 10, width = 55)
+out = scrolledtext.ScrolledText(window, height = 20, width = 70)
 out.grid(column=0,row=8)
 
 window.mainloop()
